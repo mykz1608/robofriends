782d9ca49ccee59706127c881f7e49cc9b85a688
mykz1608 <61507561+mykz1608@users.noreply.github.com>
2020-07-28 22:12:04 +0800

    Updates

---
+++ precache-manifest.6c31ba3405b90f5826c8cba084adcc77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852486a35c6564e67ce5768380d3fdcc",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d6cf6a203e59e6f48e2b",
+    "url": "/robofriends/static/css/main.f8f30f40.chunk.css"
+  },
+  {
+    "revision": "cadb83d287e325f91aec",
+    "url": "/robofriends/static/js/2.fa1425df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.fa1425df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6cf6a203e59e6f48e2b",
+    "url": "/robofriends/static/js/main.1c2bcb75.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
